010f04747c99a999c457aa36c52797ee767287bb
kabilan4017 <150924321+kabilan4017@users.noreply.github.com>
2023-11-17 23:21:57 +0530

    Add files via upload

---
+++ Task3/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to my First React App By Kabilan K
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/kabilan4017"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My GitHub Link
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
